b182f577544e6233e2da7a1d21f0569fc585922d
nikhilguptacse-29 <nikhilgupta.cse2025@citchennai.net>
2026-07-19 09:53:11 +0530

    Solved problem number 560

---
+++ Leetcode_Medium/560.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int subarraySum(vector<int> &nums, int k)
+    {
+
+        unordered_map<int, int> mp;
+
+        mp[0] = 1;
+
+        int prefixSum = 0;
+        int count = 0;
+
+        for (int num : nums)
+        {
+            prefixSum += num;
+
+            if (mp.find(prefixSum - k) != mp.end())
+            {
+                count += mp[prefixSum - k];
+            }
+
+            mp[prefixSum]++;
+        }
+
+        return count;
+    }
+};
+int main()
+{
+    Solution sol;
+    vector<int> nums = {1, 1, 1};
+    int k = 2;
+    int result = sol.subarraySum(nums, k);
+    cout << "Number of subarrays: " << result << endl;
+
+    return 0;
+}
