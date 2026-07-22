3af1f640bcf2715bb5dd3e48900a49ed9bd29c80
Nikhil-Gupta67 <nikhilgupta8595665@gmail.com>
2026-07-22 20:18:45 +0530

    Modified the  problem number 26

--- Leetcode_Easy/26.cpp
+++ Leetcode_Easy/26.cpp
@@ -23,3 +23,17 @@ public:
         return i + 1;
     }
 };
+int main()
+{
+    Solution s;
+    vector<int> input = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+    int result        = s.removeDuplicates(input);
+    cout << "Length of array after removing duplicates: " << result << endl;
+    cout << "Modified array: ";
+    for (int i = 0; i < result; i++)
+    {
+        cout << input[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
