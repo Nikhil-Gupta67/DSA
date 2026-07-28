01fa65da3983a36135f330a3e071ab4bae40fddb
Nikhil-Gupta67 <nikhilgupta8595665@gmail.com>
2026-07-28 20:42:35 +0530

    solved problem number 9

---
+++ Leetcode_Easy/9.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+
+        if(x < 0)
+            return false;
+
+        long rev = 0;
+        int temp = x;
+
+        while(temp)
+        {
+            rev = rev*10 + temp%10;
+            temp /= 10;
+        }
+
+        return rev == x;
+    }
+};
