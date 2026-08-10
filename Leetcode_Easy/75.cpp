9f5f141ef0d5c2dfc7d5a77c7e8ba37b58b0559a
Nikhil-Gupta67 <nikhilgupta8595665@gmail.com>
2026-08-10 20:17:24 +0530

    Solved the question number 75

--- Leetcode_Easy/75.cpp
+++ Leetcode_Easy/75.cpp
@@ -1,30 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    void sortColors(vector<int>& nums) {
+    void sortColors(vector<int> &nums)
+    {
 
         int low = 0;
         int mid = 0;
         int high = nums.size() - 1;
 
-        while (mid <= high) {
+        while (mid <= high)
+        {
 
-            if (nums[mid] == 0) {
+            if (nums[mid] == 0)
+            {
                 swap(nums[low], nums[mid]);
                 low++;
                 mid++;
             }
 
-            else if (nums[mid] == 1) {
+            else if (nums[mid] == 1)
+            {
                 mid++;
             }
 
-            else { // nums[mid] == 2
+            else
+            { // nums[mid] == 2
                 swap(nums[mid], nums[high]);
                 high--;
             }
         }
     }
 };
+int main()
+{
+    Solution solution;
+    vector<int> nums = {2, 0, 2, 1, 1, 0};
+    solution.sortColors(nums);
+
+    for (int num : nums)
+    {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
