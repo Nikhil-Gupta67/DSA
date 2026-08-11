4cd2e4c7288b2270e14a8b716bf8f002ed08e7ab
Nikhil-Gupta67 <nikhilgupta8595665@gmail.com>
2026-08-11 21:50:24 +0530

    Solved the question number 1491

---
+++ Leetcode_Easy/1491.cpp
@@ -0,0 +1,26 @@
+class Solution
+{
+public:
+    double average(vector<int> &salary)
+    {
+
+        int sum = 0;
+
+        int minimum = salary[0];
+        int maximum = salary[0];
+
+        for (int i = 0; i < salary.size(); i++)
+        {
+
+            sum += salary[i];
+
+            minimum = min(minimum, salary[i]);
+
+            maximum = max(maximum, salary[i]);
+        }
+
+        sum = sum - minimum - maximum;
+
+        return (double)sum / (salary.size() - 2);
+    }
+};
